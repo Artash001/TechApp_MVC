439aa36932756ba8b32e1240049214403113e30b
Artash <artashesarshakyan@gmail.com>
2023-11-19 22:09:19 +0400

    category list

--- TechApp/Controllers/CategoryController.cs
+++ TechApp/Controllers/CategoryController.cs
@@ -16,4 +16,76 @@ public class CategoryController : Controller
         List<Category> objCategoryList = _db.Categories.ToList();
         return View(objCategoryList);
     }
+
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Category obj)
+    {
+        if (obj.Name == obj.DisplayOrder.ToString())
+        {
+            ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+        }
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public IActionResult Edit(int? id)
+    {
+        if(id==null || id == 0)
+        {
+            return NotFound();
+        }
+        Category? categoryFromDb = _db.Categories.Find(id);
+        if(categoryFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+
+    [HttpPost]
+    public IActionResult Edit(Category obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category edited successfully";
+            return RedirectToAction("Index");
+        }
+        return View();
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        Category? categoryFromDb = _db.Categories.Find(id);
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(Category obj)
+    {
+        _db.Categories.Remove(obj);
+        _db.SaveChanges();
+        TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
+    }
 }
