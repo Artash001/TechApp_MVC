f2c0e16ce8e1d723055dfd2591ce6f8da88232ba
Artash001 <artashesarshakyan4@gmail.com>
2024-02-06 11:42:02 +0400

    Adding identity

--- Tech.DataAccess/Data/ApplicationDbContext.cs
+++ Tech.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Tech.Models;
 
 
 namespace Tech.DataAccess.Data;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
@@ -16,6 +17,8 @@ public class ApplicationDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+
         modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Engine", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "Transmission", DisplayOrder = 2 },
