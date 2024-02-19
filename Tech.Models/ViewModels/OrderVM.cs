ee2eaccb086765a98ca80a993ce9395ee5e5b607
Artash001 <artashesarshakyan4@gmail.com>
2024-02-19 17:38:17 +0400

    Added order status filter.

---
+++ Tech.Models/ViewModels/OrderVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tech.Models.ViewModels;
+
+public class OrderVM
+{
+    public OrderHeader OrderHeader { get; set; }
+    public IEnumerable<OrderDetail> OrderDetail { get; set; }
+}
---
+++ TechApp/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Tech.DataAccess.Repository.IRepository;
+using Tech.Models;
+using Tech.Models.ViewModels;
+using Tech.Utility;
+
+namespace TechApp.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class OrderController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public OrderController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Details(int orderId)
+    {
+        OrderVM orderVM = new()
+        {
+            OrderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == orderId, IncludeProperties: "ApplicationUser"),
+            OrderDetail = _unitOfWork.OrderDetail.GetAll(u => u.OrderHeaderId == orderId, IncludeProperties: "Product")
+        };
+
+        return View(orderVM);
+    }
+
+
+
+
+
+    [HttpGet]
+    public IActionResult GetAll(string status)
+    {
+        IEnumerable<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(IncludeProperties: "ApplicationUser").ToList();
+
+        switch (status)
+        {
+            case "pending":
+                objOrderHeaders = objOrderHeaders.Where(u => u.PaymentStatus == SD.PaymentStatusDelayedPayment);
+                break;
+            case "inprocess":
+                objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusInprocess);
+                break;
+            case "completed":
+                objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                break;
+            case "approved":
+                objOrderHeaders = objOrderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                break;
+            default:
+                break;
+        }
+
+        return Json(new { data = objOrderHeaders });
+    }
+}
