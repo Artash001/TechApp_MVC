c287200bb82b3be4ceb47a9ac159ba96167e080a
Artash <artashesarshakyan@gmail.com>
2024-03-27 16:01:45 +0400

    Finalizing Project.

--- TechApp/Areas/Customer/Controllers/CartController.cs
+++ TechApp/Areas/Customer/Controllers/CartController.cs
@@ -172,6 +172,7 @@ public class CartController : Controller
                 _unitOfWork.OrderHeader.UpdateStatus(id, SD.StatusApproved, SD.PaymentStatusApproved);
                 _unitOfWork.Save();
             }
+            HttpContext.Session.Clear();
         }
 
         List<ShoppingCart> shoppingCarts = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == orderHeader.ApplicationUserId).ToList();
